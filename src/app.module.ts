b9f4b6bcf9def5f0beb8844111d5ed6e7c98bc37
Fahim Wayez <fahimwayez1@gmail.com>
2025-05-27 03:22:25 +0600

    completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessagesModule } from './messages/messages.module';
+import { MojoMutkiController } from './mojo-mutki/mojo-mutki.controller';
+import { InventoryController } from './inventory/inventory.controller';
+import { MojoMutkiService } from './mojo-mutki/mojo-mutki.service';
+import { InventoryService } from './inventory/inventory.service';
+
+// if (!process.env.MONGO_URI) {
+//   throw new Error('MONGO_URI is not defined in the environment variables');
+// }
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        if (!process.env.MONGO_URI) {
+          throw new Error(
+            'MONGO_URI is not defined in the environment variables',
+          );
+        }
+        return { uri: process.env.MONGO_URI };
+      },
+    }),
+    MessagesModule,
+  ],
+  controllers: [MojoMutkiController, InventoryController],
+  providers: [MojoMutkiService, InventoryService],
 })
 export class AppModule {}
